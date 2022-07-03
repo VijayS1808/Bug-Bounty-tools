c145da7022af325aafb5018857aff3fe9a82203a
Vijaysutar744825 <40523063+Vijaysutar744825@users.noreply.github.com>
2022-07-03 10:04:19 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,3 +1,15 @@
+wget https://github.com/KathanP19/Gxss/releases/download/v4.1/Gxss_4.1_Linux_x86_64.tar.gz
+
+tar -xvzf Gxss_4.1_Linux_i386.tar.gz
+
+mv -v Gxss /usr/bin
+
+rm -rf Gxss_4.1_Linux_i386.tar.gz
+
+cd $HOME
+
+
+
 toilet XSS --gay
 
 echo -e "\e[1;31;47m "Enter the domain::: " \e[1m" 
