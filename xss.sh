8d9a9655b988db09c31c7305d97ac3b5c9a6ee14
VijayS1808 <40523063+VijayS1808@users.noreply.github.com>
2022-07-09 10:48:40 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,11 +1,13 @@
 go get -u github.com/tomnomnom/gf
 cd gopath/bin
 mv -v gf /usr/bin
-cd 
+cd
 wget https://github.com/KathanP19/Gxss/releases/download/v4.1/Gxss_4.1_Linux_x86_64.tar.gz
 tar -xvf Gxss_4.1_Linux_x86_64.tar.gz
 mv -v Gxss /usr/bin
-cd 
+cd
+
+pip3 install uro
 
 
 
@@ -13,11 +15,16 @@ cd
 
 toilet XSS --gay
 
-echo -e "\e[1;31;47m "Enter the domain::: " \e[1m" 
+echo -e "\e[1;31;47m "Enter the domain::: " \e[1m"
 
 read s
 
-waybackurls $s | gf xss | sed 's/=.*/=/' | sort -u | uniq | tee urls.txt
+waybackurls $s | grep "=" | gf xss | sort -u | tee urls.txt
 
-tee urls.txt | Gxss -p test | dalfox pipe --silence --skip-mining-dict --skip-mining-all >> output.txt
-rm urls.txt
+cat urls.txt | httpx -mc 200 | tee 200.txt
+
+tee 200.txt | uro | Gxss -p test | tee dalfox.txt
+cat dalfox.txt | dalfox pipe --silence --skip-mining-dict --skip-mining-all  | tee out.txt
+
+rm -rf urls.txt 200.txt dalfox.txt
+
